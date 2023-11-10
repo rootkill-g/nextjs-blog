bee7350156111d15ae34cb959861568ab74bc83d
rootkill <rootkill.dedsec@gmail.com>
2023-11-10 20:11:12 +0530

    Updated the name property

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'rootkill';
+const name = 'rootkill-g';
 export const siteTitle = 'Ghidra';
 
 export default function Layout({ children, home }) {
